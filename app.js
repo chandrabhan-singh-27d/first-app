003b765ec2fb43ba6303fdbc644328eeac8052fa
chandrabhan <chandrabhansingh27d@gmail.com>
2024-01-23 20:07:08 +0530

    files added

--- app.js
+++ app.js
@@ -1,44 +1,18 @@
 import express from "express";
-import logger from "./logger.js";
-import authorize from "./authorize.js";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const people = require("./people.json");
 
 const app = express();
 
-// req => middleware => res
-app.use(logger);
-app.use('/sensitive', authorize)
 
-/* 
-- instead of using this logger middleware in every route, we can use app.use so that it is available to each of the routes. 
-app.get('/', logger, (req, res) => {
-    res.send(`
-    <h1>Home Page</h1>
-    `)
-}); 
-*/
+app.get('/people', (req, res) => {
+    let requestedPeople = [...people];
+    const { limit } = req.query;
 
-app.get('/', (req, res) => {
-    res.send(`
-    <h1>Home Page</h1>
-    `)
-});
+    requestedPeople = requestedPeople.slice(0, Number(limit));
 
-app.get('/about', (req, res) => {
-    res.send(`
-    <h1>About Page</h1>
-    <a href="/">Back Home</a>
-    `)
-});
+    res.status(200).json(requestedPeople);
+})
 
-app.get('/sensitive', (req, res) => {
-    res.status(200).send(`
-    <p>Hello sir, kar lijiye apni manmaani</p>
-    <a href="/">Back Home</a>
-    `)
-});
-
-
-
-app.listen(5000, () => {
-    console.log("Server listening at port 5000...");
-});
+app.listen(5000, () => console.log("server listening on port 5000")); 
--- middleware.js
+++ middleware.js
@@ -32,3 +32,51 @@ app.get('/about', logger, (req, res) => {
 app.listen(5000, () => {
     console.log("Server listening at port 5000...");
 });
+
+// modified version
+
+
+import express from "express";
+import logger from "./logger.js";
+import authorize from "./authorize.js";
+
+const app1 = express();
+
+// req => middleware => res
+app1.use(logger);
+app1.use('/sensitive', authorize)
+
+/* 
+- instead of using this logger middleware in every route, we can use app.use so that it is available to each of the routes. 
+app.get('/', logger, (req, res) => {
+    res.send(`
+    <h1>Home Page</h1>
+    `)
+}); 
+*/
+
+app1.get('/', (req, res) => {
+    res.send(`
+    <h1>Home Page</h1>
+    `)
+});
+
+app1.get('/about', (req, res) => {
+    res.send(`
+    <h1>About Page</h1>
+    <a href="/">Back Home</a>
+    `)
+});
+
+app1.get('/sensitive', (req, res) => {
+    res.status(200).send(`
+    <p>Hello sir, kar lijiye apni manmaani</p>
+    <a href="/">Back Home</a>
+    `)
+});
+
+
+
+app1.listen(5000, () => {
+    console.log("Server listening at port 5000...");
+});
